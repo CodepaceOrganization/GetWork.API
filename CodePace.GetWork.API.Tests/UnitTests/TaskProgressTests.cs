7b0abcc006871105332110ac0d3af7b5e84a53e9
Adriano Sebastian Cruz Palomino <u202210697@upc.edu.pe>
2024-06-09 01:07:59 -0500

    feat: added unit test of task progress and technical task

---
+++ CodePace.GetWork.API.Tests/UnitTests/TaskProgressTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using CodePace.GetWork.API;
+using CodePace.GetWork.API.TechnicalEvaluation.Domain.Model.Entities;
+using CodePace.GetWork.API.TechnicalEvaluation.Domain.Model.ValueObjects;
+
+namespace CodePace.GetWork.Tests
+{
+    [TestFixture]
+    public class TaskProgressTests
+    {
+        [Test]
+        public void Constructor_Default_ShouldInitializeWithDefaults()
+        {
+            // Arrange & Act
+            var taskProgress = new TaskProgress();
+
+            // Assert
+            Assert.AreEqual(0, taskProgress.UserId);
+            Assert.AreEqual(EProgress.Earrings, taskProgress.Progress);
+            Assert.AreEqual(0, taskProgress.TechnicalTaskId);
+            Assert.IsNull(taskProgress.TechnicalTask);
+        }
+
+        [Test]
+        public void Constructor_Parameterized_ShouldInitializeWithGivenValues()
+        {
+            // Arrange
+            int userId = 1;
+            int technicalTaskId = 2;
+
+            // Act
+            var taskProgress = new TaskProgress(userId, technicalTaskId);
+
+            // Assert
+            Assert.AreEqual(userId, taskProgress.UserId);
+            Assert.AreEqual(technicalTaskId, taskProgress.TechnicalTaskId);
+            Assert.AreEqual(EProgress.Earrings, taskProgress.Progress);
+            Assert.IsNull(taskProgress.TechnicalTask);
+        }
+
+        [Test]
+        public void UpdateUserId_ShouldChangeUserId()
+        {
+            // Arrange
+            var taskProgress = new TaskProgress();
+            int newUserId = 3;
+
+            // Act
+            taskProgress.UpdateUserId(newUserId);
+
+            // Assert
+            Assert.AreEqual(newUserId, taskProgress.UserId);
+        }
+
+        [Test]
+        public void UpdateProgress_ShouldChangeProgress()
+        {
+            // Arrange
+            var taskProgress = new TaskProgress();
+            EProgress newProgress = EProgress.InProgress;
+
+            // Act
+            taskProgress.UpdateProgress(newProgress);
+
+            // Assert
+            Assert.AreEqual(newProgress, taskProgress.Progress);
+        }
+    }
+}
---
+++ CodePace.GetWork.API.Tests/UnitTests/TechnicalTask.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using CodePace.GetWork.API.TechnicalEvaluation.Domain.Model.Entities;
+using CodePace.GetWork.API.TechnicalEvaluation.Domain.Model.ValueObjects;
+
+namespace CodePace.GetWork.Tests
+{
+    [TestFixture]
+    public class TechnicalTaskTests
+    {
+        [Test]
+        public void Constructor_Default_ShouldInitializeWithDefaults()
+        {
+            // Arrange & Act
+            var technicalTask = new TechnicalTask();
+
+            // Assert
+            Assert.AreEqual(string.Empty, technicalTask.Description);
+            Assert.AreEqual(EDificultyStatus.Easy, technicalTask.Difficulty);
+            Assert.NotNull(technicalTask.TaskProgress);
+            Assert.AreEqual(0, technicalTask.TaskProgress.TechnicalTaskId);
+        }
+
+        [Test]
+        public void Constructor_Parameterized_ShouldInitializeWithGivenValues()
+        {
+            // Arrange
+            string description = "Sample task";
+            EDificultyStatus difficulty = EDificultyStatus.Medium;
+
+            // Act
+            var technicalTask = new TechnicalTask(description, difficulty);
+
+            // Assert
+            Assert.AreEqual(description, technicalTask.Description);
+            Assert.AreEqual(difficulty, technicalTask.Difficulty);
+            Assert.NotNull(technicalTask.TaskProgress);
+            Assert.AreEqual(0, technicalTask.TaskProgress.TechnicalTaskId);
+        }
+
+        [Test]
+        public void UpdateDescription_ShouldChangeDescription()
+        {
+            // Arrange
+            var technicalTask = new TechnicalTask();
+            string newDescription = "Updated task description";
+
+            // Act
+            technicalTask.UpdateDescription(newDescription);
+
+            // Assert
+            Assert.AreEqual(newDescription, technicalTask.Description);
+        }
+
+        [Test]
+        public void UpdateDifficulty_ShouldChangeDifficulty()
+        {
+            // Arrange
+            var technicalTask = new TechnicalTask();
+            EDificultyStatus newDifficulty = EDificultyStatus.Hard;
+
+            // Act
+            technicalTask.UpdateDifficulty(newDifficulty);
+
+            // Assert
+            Assert.AreEqual(newDifficulty, technicalTask.Difficulty);
+        }
+    }
+}
